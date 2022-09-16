64e1c7f4018743844dcc330b0857bea7216aa470
Josue <Josue_0096@live.it>
2022-09-16 21:40:41 +0200

    calcolatore: rimozione della pagina statica "index.html". L'applicazione funziona solo tramite servlet

--- servlet/webhello/src/main/java/webhello/CalcServlet.java
+++ servlet/webhello/src/main/java/webhello/CalcServlet.java
@@ -11,18 +11,22 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-public class HelloServlet extends HttpServlet {
+public class CalcServlet extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 
 		PrintWriter w = res.getWriter();
-		w.print("<html><body><b>hai chiamato la get</b><hr/>");
-		
-		//posso mettere i parametri direttamente nella barra deglsi indirizzi. ese: ?param1=ciao!&param2=addio!
-		for(String att: req.getParameterMap().keySet())
-			w.print(att + "=" + req.getParameter(att) + "<hr/>");
-		w.print("</body></html>");
+		w.print("<html><body><b>Calcolatore</b><hr/>"
+				+ "	<form action=\"calcolatrice\" method=\"post\">\n"
+				+ "	  <input type=\"text\" id=\"1op\" name=\"1op\"><br>\n"
+				+ "	  <input type=\"text\" id=\"2op\" name=\"2op\"><br><br>\n"
+				+ "	  <input type=\"submit\" value=\"somma\" name=\"op\">\n"
+				+ "	  <input type=\"submit\" value=\"sottrazione\" name=\"op\">\n"
+				+ "	  <input type=\"submit\" value=\"prodotto\" name=\"op\">\n"
+				+ "	  <input type=\"submit\" value=\"divisione\" name=\"op\">\n"
+				+ "	</form><hr/>"
+				+ "</body></html>");
 	}
 	
 	@Override
@@ -48,6 +52,8 @@ public class HelloServlet extends HttpServlet {
 			result = number1 / number2;
 			break;
 		}
+		
+		doGet(req, res);
 		
 		PrintWriter w = res.getWriter();
 		w.print("<html><body>risultato = <b>");
