e91adb9eb91a40d37d934910dd45571420509c10
Josue <Josue_0096@live.it>
2022-10-24 17:40:40 +0200

    metodi per la visualizzazione su console dei record delle entità

--- database/JDBCProject/src/main/java/jdbctest/Main.java
+++ database/JDBCProject/src/main/java/jdbctest/Main.java
@@ -1,12 +1,22 @@
 package jdbctest;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Main {
 
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
 
-		Utils.connectionDB();
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		String url = "jdbc:mysql://localhost:3306/JdbcSchema?user=root&passowrd=PuchiPuchi25!";
+		Connection dbConnection = DriverManager.getConnection(url, "root", "PuchiPuchi25!");
+		System.out.println("connessione aperta");
+		
+		Utils.listaAlunni(dbConnection);
+		Utils.listaProfessori(dbConnection);
+		Utils.materie(dbConnection);
+		
 	}
 
 }
--- database/JDBCProject/src/main/java/jdbctest/Utils.java
+++ database/JDBCProject/src/main/java/jdbctest/Utils.java
@@ -2,15 +2,61 @@ package jdbctest;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class Utils {
-
-	static void connectionDB() throws ClassNotFoundException, SQLException {
+	
+	static void listaAlunni(Connection dbConnection) throws SQLException {
 		
-		Class.forName("com.mysql.cj.jdbc.Driver");
-		String url = "jdbc:mysql://localhost:3306/JdbcSchema?user=root&passowrd=PuchiPuchi25!";
-		Connection dbConnection = DriverManager.getConnection(url, "root", "PuchiPuchi25!");
-		System.out.println("connessione aperta");
+		String query = "SELECT nombre, apellidos, fecha_nac\n"
+				+ "FROM JdbcSchema.alumnos;";
+		PreparedStatement cmd = dbConnection.prepareStatement(query);
+		ResultSet res = cmd.executeQuery();
+		
+		boolean esiste = res.next();
+		while(esiste) {
+			
+			String nome = res.getString("nombre");
+			String cognome = res.getString("apellidos");
+			String dataNascita = res.getString("fecha_nac");
+			System.out.println("nome: "+nome+" cognome: "+cognome+" data di nascita: "+dataNascita);
+			esiste = res.next();
+		}
+	}
+	
+	static void listaProfessori(Connection dbConnection) throws SQLException {
+		
+		String query = "SELECT nombre, apellidos\n"
+				+ "FROM JdbcSchema.profesores;";
+		PreparedStatement cmd = dbConnection.prepareStatement(query);
+		ResultSet res = cmd.executeQuery();
+		
+		boolean esiste = res.next();
+		while(esiste) {
+			
+			String nome = res.getString("nombre");
+			String cognome = res.getString("apellidos");
+			System.out.println("nome: "+nome+" cognome: "+cognome);
+			esiste = res.next();
+		}
+	}
+	
+	static void materie(Connection dbConnection) throws SQLException {
+		
+		String query = "SELECT nombre, profesor\n"
+				+ "FROM JdbcSchema.asignaturas";
+		PreparedStatement cmd = dbConnection.prepareStatement(query);
+		ResultSet res = cmd.executeQuery();
+		
+		boolean esiste = res.next();
+		while(esiste) {
+			
+			String nome = res.getString("nombre");
+			String prof = res.getString("profesor");
+			System.out.println("nome: "+nome+" professore: "+prof);
+			esiste = res.next();
+		}
 	}
 }
