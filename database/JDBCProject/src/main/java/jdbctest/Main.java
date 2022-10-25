9c9bcdd731f8eb50799540845bae7437b57b28d0
Josue <Josue_0096@live.it>
2022-10-25 14:43:36 +0200

    metodi con parametri per visualizzare su console un record specifico

--- database/JDBCProject/src/main/java/jdbctest/Main.java
+++ database/JDBCProject/src/main/java/jdbctest/Main.java
@@ -8,14 +8,24 @@ public class Main {
 
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
 
-		Class.forName("com.mysql.cj.jdbc.Driver");
-		String url = "jdbc:mysql://localhost:3306/JdbcSchema?user=root&passowrd=PuchiPuchi25!";
-		Connection dbConnection = DriverManager.getConnection(url, "root", "PuchiPuchi25!");
-		System.out.println("connessione aperta");
+		Connection dbConnection = null;
 		
-		Utils.listaAlunni(dbConnection);
-		Utils.listaProfessori(dbConnection);
-		Utils.materie(dbConnection);
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			String url = "jdbc:mysql://localhost:3306/JdbcSchema?user=root&passowrd=PuchiPuchi25!";
+			dbConnection = DriverManager.getConnection(url, "root", "PuchiPuchi25!");
+			System.out.println("connessione aperta");
+			
+			Utils.cercaAlunno(dbConnection, "Ana");
+			Utils.cercaProfessore(dbConnection, "Agustín");
+			Utils.materie(dbConnection);
+			Utils.matricole(dbConnection);
+			Utils.cercaMatricola(dbConnection, 3);
+		} catch (SQLException e) {
+			e.getMessage();
+		} finally {
+			dbConnection.close();
+		}
 		
 	}
 
--- database/JDBCProject/src/main/java/jdbctest/Utils.java
+++ database/JDBCProject/src/main/java/jdbctest/Utils.java
@@ -8,55 +8,108 @@ import java.sql.SQLException;
 
 public class Utils {
 	
-	static void listaAlunni(Connection dbConnection) throws SQLException {
+	static void cercaAlunno(Connection dbConnection, String n) throws SQLException {
 		
 		String query = "SELECT nombre, apellidos, fecha_nac\n"
-				+ "FROM JdbcSchema.alumnos;";
+				+ "FROM JdbcSchema.alumnos\n"
+				+ "WHERE nombre= ?;";
 		PreparedStatement cmd = dbConnection.prepareStatement(query);
+		cmd.setString(1,n);
 		ResultSet res = cmd.executeQuery();
 		
 		boolean esiste = res.next();
-		while(esiste) {
 			
-			String nome = res.getString("nombre");
-			String cognome = res.getString("apellidos");
-			String dataNascita = res.getString("fecha_nac");
-			System.out.println("nome: "+nome+" cognome: "+cognome+" data di nascita: "+dataNascita);
-			esiste = res.next();
-		}
+		String nome = res.getString("nombre");
+		String cognome = res.getString("apellidos");
+		String dataNascita = res.getString("fecha_nac");
+		System.out.println("nome: "+nome+" cognome: "+cognome+" data di nascita: "+dataNascita);
+			
+		cmd.close();
+		res.close();
 	}
 	
-	static void listaProfessori(Connection dbConnection) throws SQLException {
+	static void cercaProfessore(Connection dbConnection, String n) throws SQLException {
 		
 		String query = "SELECT nombre, apellidos\n"
-				+ "FROM JdbcSchema.profesores;";
+				+ "FROM JdbcSchema.profesores\n"
+				+ "WHERE nombre= ?";
 		PreparedStatement cmd = dbConnection.prepareStatement(query);
+		cmd.setString(1, n);
 		ResultSet res = cmd.executeQuery();
 		
 		boolean esiste = res.next();
-		while(esiste) {
 			
-			String nome = res.getString("nombre");
-			String cognome = res.getString("apellidos");
-			System.out.println("nome: "+nome+" cognome: "+cognome);
-			esiste = res.next();
-		}
+		String nome = res.getString("nombre");
+		String cognome = res.getString("apellidos");
+		System.out.println("nome: "+nome+" cognome: "+cognome);
+		
+		cmd.close();
+		res.close();
 	}
 	
 	static void materie(Connection dbConnection) throws SQLException {
 		
-		String query = "SELECT nombre, profesor\n"
-				+ "FROM JdbcSchema.asignaturas";
+		String query = "SELECT asignaturas.nombre, profesores.nombre, profesores.apellidos\n"
+				+ "FROM JdbcSchema.asignaturas, profesores\n"
+				+ "WHERE asignaturas.profesor=profesores.id_profesor";
 		PreparedStatement cmd = dbConnection.prepareStatement(query);
 		ResultSet res = cmd.executeQuery();
 		
 		boolean esiste = res.next();
 		while(esiste) {
 			
-			String nome = res.getString("nombre");
-			String prof = res.getString("profesor");
-			System.out.println("nome: "+nome+" professore: "+prof);
+			String nomeM = res.getString("nombre");
+			String nome = res.getString("profesores.nombre");
+			String cognome = res.getString("apellidos");
+			System.out.println("nome-materia: "+nomeM+" nome-prof: "+nome+" cognome-prof: "+cognome);
 			esiste = res.next();
 		}
+		cmd.close();
+		res.close();
+	}
+	
+	static void matricole(Connection dbConnection) throws SQLException {
+		
+		String query = "SELECT alumno, asignatura, fecha, nota\n"
+				+ "FROM JdbcSchema.matriculas";
+		PreparedStatement cmd = dbConnection.prepareStatement(query);
+		ResultSet res = cmd.executeQuery();
+		
+		boolean esiste = res.next();
+		while(esiste) {
+			
+			String alunno = res.getString("alumno");
+			String assegnatura = res.getString("asignatura");
+			String data = res.getString("fecha");
+			int nota = res.getInt("nota");
+			System.out.println("alunno: "+alunno+" assegnatura: "+assegnatura+" voto: "+nota);
+			esiste = res.next();
+		}
+		cmd.close();
+		res.close();
+	}
+	
+	static void cercaMatricola(Connection dbConnection, int i) throws SQLException {
+		
+		String query = "SELECT alumnos.nombre, alumnos.apellidos, asignaturas.nombre, profesores.nombre, profesores.apellidos, matriculas.fecha, matriculas.nota\n"
+				+ "FROM alumnos, matriculas, asignaturas, profesores\n"
+				+ "WHERE matriculas.alumno = ? AND matriculas.alumno = alumnos.id_alumno AND matriculas.asignatura = asignaturas.id_asignatura AND asignaturas.profesor = profesores.id_profesor;";
+		PreparedStatement cmd = dbConnection.prepareStatement(query);
+		cmd.setInt(1,i);
+		ResultSet res = cmd.executeQuery();
+		
+		boolean esiste = res.next();
+		String nomeAlunno = res.getString("alumnos.nombre");
+		String cognomeAlunno = res.getString("alumnos.apellidos");
+		String nomeMateria = res.getString("asignaturas.nombre");
+		String nomeProf = res.getString("profesores.nombre");
+		String cognomeProf = res.getString("profesores.apellidos");
+		int data = res.getInt("matriculas.fecha");
+		int voto = res.getInt("matriculas.nota");
+
+		System.out.println("nome-alunno: "+nomeAlunno+" cognome-alunno: "+cognomeAlunno+" nome-materia: "+nomeMateria+" nome-prof: "+nomeProf+" cognome-prof: "+cognomeProf+" data-voto: "+data+" voto: "+voto);
+	
+		cmd.close();
+		res.close();
 	}
 }
