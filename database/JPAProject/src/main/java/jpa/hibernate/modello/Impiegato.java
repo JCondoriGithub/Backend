c72cdcba63e927edff3d6201088b6ff60c20554b
Josue <Josue_0096@live.it>
2022-11-01 22:12:26 +0100

    creazione di un "Entity"

---
+++ database/JPAProject/src/main/java/jpa/hibernate/modello/Impiegato.java
@@ -0,0 +1,74 @@
+package jpa.hibernate.modello;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tbl_Impiegato")
+public class Impiegato {
+
+	@Id
+	@Column(name = "code_impiegato")
+	private int codice;
+	
+	@Column(name = "nome")
+	private String nome;
+	
+	@Column(name = "cognome")
+	private String cognome;
+	
+	@Column(name = "data_nascita")
+	private String dataNascita;
+	
+	public Impiegato() {
+		
+	}
+
+	public Impiegato(int codice, String nome, String cognome, String dataNascita) {
+		this.codice = codice;
+		this.nome = nome;
+		this.cognome = cognome;
+		this.dataNascita = dataNascita;
+	}
+
+	public int getCodice() {
+		return codice;
+	}
+
+	public void setCodice(int codice) {
+		this.codice = codice;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCognome() {
+		return cognome;
+	}
+
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+
+	public String getDataNascita() {
+		return dataNascita;
+	}
+
+	public void setDataNascita(String dataNascita) {
+		this.dataNascita = dataNascita;
+	}
+
+	@Override
+	public String toString() {
+		return "Impiegato [codice=" + codice + ", nome=" + nome + ", cognome=" + cognome + ", dataNascita="
+				+ dataNascita + "]";
+	}
+	
+}
